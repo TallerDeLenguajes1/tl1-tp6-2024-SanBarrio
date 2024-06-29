0590ecb6eb4bc13a2d47c187218e3e510573b163
SantiB <santi.barrionuevo411@gmail.com>
2024-06-29 20:37:09 -0300

    Realizado el Ejercicio 3

--- Program.cs
+++ Program.cs
@@ -1,51 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Reflection.Metadata.Ecma335;
 
-int i;
-do{
-Console.WriteLine("¿Que operación desea realizar?\n1)Sumar\n2)Restar\n3)Multiplicar\n4)Dividir\n5)Salir.");
+float i;
+
+Console.WriteLine("Ingrese un valor:");
 i = tomarValor();
-int a,b;
-switch (i)
-{
-    case 1: 
-        Console.WriteLine("Ingrese el primer valor: ");
-         a=tomarValor();
-        Console.WriteLine("Ingrese el segundo valor: ");
-        b=tomarValor();
-        Console.WriteLine($"La suma de {a} + {b} es: {a+b}");
-    break;
-      case 2: 
-        Console.WriteLine("Ingrese el primer valor: ");
-        a=tomarValor();
-        Console.WriteLine("Ingrese el segundo valor: ");
-        b=tomarValor();
-        Console.WriteLine($"La resta de {a} - {b} es: {a-b}");
-    break;
-      case 3: 
-        Console.WriteLine("Ingrese el primer valor: ");
-        a=tomarValor();
-        Console.WriteLine("Ingrese el segundo valor: ");
-        b=tomarValor();
-        Console.WriteLine($"La multiplicacion de {a} x {b} es: {a*b}");
-    break;
-      case 4: 
-        Console.WriteLine("Ingrese el primer valor: ");
-        a=tomarValor();
-        Console.WriteLine("Ingrese el segundo valor, que sea distinto a 0: ");
-        b=tomarValor();
-        Console.WriteLine($"La division de {a} y {b} es: {a/b}");
-    break;
-    default: break;
-}
-
-}while(i!=5);
+Console.WriteLine($"Valor Absoluto de {i}: {Math.Abs(i)}");
+Console.WriteLine($"Cuadrado de {i}: {Math.Pow(i,2)}");
+Console.WriteLine($"Raiz cuadrada de {i}: {Math.Sqrt(i)}");
+Console.WriteLine($"Seno de {i}: {Math.Sin(i)}");
+Console.WriteLine($"Coseno de {i}: {Math.Cos(i)}");
+Console.WriteLine($"Parte entera de {i}: {(int)i}");
 
 
-int tomarValor(){
-    int a;
+float a,b;
+Console.WriteLine("\n-------- Maximo y Minimo -----------\n");
+Console.WriteLine("Ingrese el primer valor: ");
+a = tomarValor();
+Console.WriteLine("Ingrese el segundo valor: ");
+b = tomarValor();
+
+Console.WriteLine($"El Maximo entre {a} y {b} es: {Math.Max(a,b)}");
+Console.WriteLine($"El Minimo entre {a} y {b} es: {Math.Min(a,b)}");
+
+float tomarValor(){
+    float a;
     while(true){
-        if(int.TryParse(Console.ReadLine(),out a)){       
+        if(float.TryParse(Console.ReadLine(),out a)){       
        return a;
     }else{
         Console.WriteLine("El valor ingresado no es un número, ingrese nuevamente: ");
