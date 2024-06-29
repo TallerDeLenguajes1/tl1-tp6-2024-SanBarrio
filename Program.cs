e4716714dfc3f21bbeb628ea975bbf9d29b1ae92
SantiB <santi.barrionuevo411@gmail.com>
2024-06-29 20:21:44 -0300

    nose

--- Program.cs
+++ Program.cs
@@ -1,15 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
 
-Console.WriteLine("Ingrese un número mayor a 0");
-int numero;
-string texto = Console.ReadLine();
-if (int.TryParse(texto, out numero) && numero > 0)
+int i;
+do{
+Console.WriteLine("¿Que operación desea realizar?\n1)Sumar\n2)Restar\n3)Multiplicar\n4)Dividir\n5)Salir.");
+i = tomarValor();
+int a,b;
+switch (i)
 {
-    char[] cadena = texto.ToCharArray();
-    Array.Reverse(cadena);
-    Console.WriteLine(cadena);
+    case 1: 
+        Console.WriteLine("Ingrese el primer valor: ");
+         a=tomarValor();
+        Console.WriteLine("Ingrese el segundo valor: ");
+        b=tomarValor();
+        Console.WriteLine($"La suma de {a} + {b} es: {a+b}");
+    break;
+      case 2: 
+        Console.WriteLine("Ingrese el primer valor: ");
+        a=tomarValor();
+        Console.WriteLine("Ingrese el segundo valor: ");
+        b=tomarValor();
+        Console.WriteLine($"La resta de {a} - {b} es: {a-b}");
+    break;
+      case 3: 
+        Console.WriteLine("Ingrese el primer valor: ");
+        a=tomarValor();
+        Console.WriteLine("Ingrese el segundo valor: ");
+        b=tomarValor();
+        Console.WriteLine($"La multiplicacion de {a} x {b} es: {a*b}");
+    break;
+      case 4: 
+        Console.WriteLine("Ingrese el primer valor: ");
+        a=tomarValor();
+        Console.WriteLine("Ingrese el segundo valor, que sea distinto a 0: ");
+        b=tomarValor();
+        Console.WriteLine($"La division de {a} y {b} es: {a/b}");
+    break;
+    default: break;
 }
-else
-{
-    Console.WriteLine("Número ingresado inválido");
+
+}while(i!=5);
+
+
+int tomarValor(){
+    int a;
+    while(true){
+        if(int.TryParse(Console.ReadLine(),out a)){       
+       return a;
+    }else{
+        Console.WriteLine("El valor ingresado no es un número, ingrese nuevamente: ");
+    }
+    }
+    
 }
