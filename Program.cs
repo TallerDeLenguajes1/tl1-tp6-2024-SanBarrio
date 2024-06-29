ab48751ece8ad404004887dc77697ce9a65ecc5d
SantiB <santi.barrionuevo411@gmail.com>
2024-06-29 20:09:06 -0300

    Realizado el Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+string i;
+do{
+Console.WriteLine("¿Que operación desea realizar?\n1)Sumar\n2)Restar\n3)Multiplicar\n4)Dividir\n5)Salir.");
+i = Console.ReadLine();
+int a,b;
+switch (i)
+{
+    case "1": 
+        Console.WriteLine("Ingrese el primer valor: ");
+        int.TryParse(Console.ReadLine(),out a);
+        Console.WriteLine("Ingrese el segundo valor: ");
+        int.TryParse(Console.ReadLine(),out b);
+        Console.WriteLine($"La suma de {a} + {b} es: {a+b}");
+    break;
+      case "2": 
+        Console.WriteLine("Ingrese el primer valor: ");
+        int.TryParse(Console.ReadLine(),out a);
+        Console.WriteLine("Ingrese el segundo valor: ");
+       int.TryParse(Console.ReadLine(),out b);
+        Console.WriteLine($"La resta de {a} - {b} es: {a-b}");
+    break;
+      case "3": 
+        Console.WriteLine("Ingrese el primer valor: ");
+        int.TryParse(Console.ReadLine(),out a);
+        Console.WriteLine("Ingrese el segundo valor: ");
+        int.TryParse(Console.ReadLine(),out b);
+        Console.WriteLine($"La multiplicacion de {a} x {b} es: {a*b}");
+    break;
+      case "4": 
+        Console.WriteLine("Ingrese el primer valor: ");
+        int.TryParse(Console.ReadLine(),out a);
+        Console.WriteLine("Ingrese el segundo valor, que sea distinto a 0: ");
+        int.TryParse(Console.ReadLine(),out b);
+        Console.WriteLine($"La division de {a} y {b} es: {a/b}");
+    break;
+    default: break;
+}
 
-Console.WriteLine("Ingrese un número mayor a 0");
-int numero;
-string texto = Console.ReadLine();
-if (int.TryParse(texto, out numero) && numero > 0)
-{
-    char[] cadena = texto.ToCharArray();
-    Array.Reverse(cadena);
-    Console.WriteLine(cadena);
-}
-else
-{
-    Console.WriteLine("Número ingresado inválido");
-}
+}while(i!="5");
