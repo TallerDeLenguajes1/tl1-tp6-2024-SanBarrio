11b6d876e0f00e7ec3949c8a05350fc1a47b310b
SantiB <santi.barrionuevo411@gmail.com>
2024-06-29 19:42:08 -0300

    Realizado el Ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-int a;
-
-int b;
-
-a=10;
-
-b=a;
-
-Console.WriteLine("valor de a: "+a);
-
-Console.WriteLine("valor de b: "+b);
+Console.WriteLine("Ingrese un número mayor a 0");
+int numero;
+string texto = Console.ReadLine();
+if (int.TryParse(texto, out numero) && numero > 0)
+{
+    char[] cadena = texto.ToCharArray();
+    Array.Reverse(cadena);
+    Console.WriteLine(cadena);
+}
+else
+{
+    Console.WriteLine("Número ingresado inválido");
+}
